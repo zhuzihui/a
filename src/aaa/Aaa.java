67e9cf0532fa1c0a9db769fb4961c302d1b98b17
lzc <775683021@qq.com>
2018-08-16 21:11:23 +0800

    大幅上升

--- src/aaa/Aaa.java
+++ src/aaa/Aaa.java
@@ -4,6 +4,7 @@ public class Aaa {
 	public static void main(String[] args) {
 		System.out.println("12nj");
 		System.out.println("qlw");     
-		System.out.println("Öì×Ó»Ô");
+
+		System.out.println("Öì×Ó»Ô");System.out.println("Öì×Ó»Ô");
 	}
 }
